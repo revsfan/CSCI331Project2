897702d369108951e55d71fa9ccec2ad198f95f3
revsfan <kaal1101@stcloudstate.edu>
2016-11-09 18:47:02 -0600

    Add files via upload
    6:46 pm
    11/9/2016

--- src/ReplacementSelectionSort.cpp
+++ src/ReplacementSelectionSort.cpp
@@ -6,23 +6,26 @@ template <typename T>
 std::vector< std::vector <T> > ReplacementSelectionSort<T>::sort(const std::vector<T> vectorToBeSorted, const int heapSize, const bool sortDirection){
 
 
-    if (heapSize < 1) 
-	{
+    if (heapSize < 1) {
         //error
     }
+
 
     std::vector< std::vector<T> > *outputVector = new std::vector< std::vector<T> >;
     DualHeap<T> *dualHeap = new DualHeap<T>( heapSize, sortDirection );
 
-    T element;
+    T   element;
     int totalCap = dualHeap -> get_cap();
     int nextItemToSort;
 
-    for (int i = 1; i < totalCap && i < vectorToBeSorted.size(); i++)
-	{
+
+    for (int i = 0; i < totalCap && i < vectorToBeSorted.size()-1; i++){
+
           element = vectorToBeSorted[i];
-		  dualHeap ->activeHeapPush( element );
-		  dualHeap = i + 1;
+
+          dualHeap ->activeHeapPush( element );
+
+          nextItemToSort = i + 1;
     }
 
     int currentList = 0;
@@ -31,48 +34,44 @@ std::vector< std::vector <T> > ReplacementSelectionSort<T>::sort(const std::vect
 
     std::vector <T> *row = new std::vector<T>;
 
-    while (nextItemToSort < vectorToBeSorted.size() || dualHeap->get_size() > 0) 
-	{
-		lastElementPopped = dualHeap->current_heap_pop();
-		row->push_back( lastElementPopped );
-		if ( nextItemToSort < vectorToBeSorted.size() ) 
-		{
-			nextElement = vectorToBeSorted[nextItemToSort];
-			nextItemToSort++;
-			if (sortDirection) 
-			{
-				if (nextElement < lastElementPopped) 
-				{
-					dualHeap->pendingHeapPush( nextElement );
-				} 
-				else 
-				{
-					dualHeap->activeHeapPush( nextElement );
+    while (nextItemToSort < vectorToBeSorted.size() || dualHeap->get_size() > 0) {
+
+
+            lastElementPopped = dualHeap->active_heap_pop();
+
+
+            row->push_back( lastElementPopped );
+
+            if ( nextItemToSort < vectorToBeSorted.size() ) {
+                nextElement = vectorToBeSorted[nextItemToSort];
+                nextItemToSort++;
+
+
+                if (sortDirection) {
+                    if (nextElement < lastElementPopped) {
+                        dualHeap->pendingHeapPush( nextElement );
+                    } else {
+                        dualHeap->activeHeapPush( nextElement );
+                    }
+                } else {
+                    if (nextElement > lastElementPopped) {
+                        dualHeap->pendingHeapPush( nextElement );
+                    } else {
+                        dualHeap->activeHeapPush( nextElement );
+                    }
                 }
-            } 
-			else 
-			{
-				if (nextElement > lastElementPopped) 
-				{
-					dualHeap->pendingHeapPush( nextElement );
-                } 
-				else 
-				{
-					dualHeap->activeHeapPush( nextElement );
-				}
+
             }
 
+            if (dualHeap->active_heap_size() == 0) {
+
+                dualHeap->changeDirection();
+                currentList++;
+                outputVector->push_back( *row );
+                row = new std::vector<T>;
+            }
         }
 
-        if (dualHeap->active_heap_size() == 0) 
-		{
-			dualHeap->changeDirection();
-            currentList++;
-            outputVector->push_back( *row );
-            row = new std::vector<T>;
-        }
-		
-    }
         delete dualHeap;
 
         return *outputVector;
