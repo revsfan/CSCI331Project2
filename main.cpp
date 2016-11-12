4276e204bf8d4e45ea5b0176093f00fbb19215d6
Steven K Scholz <stevenkurtscholz@gmail.com>
2016-11-12 17:19:42 -0600

    selectionsort 1

--- main.cpp
+++ main.cpp
@@ -1,73 +1,142 @@
-#include <iostream>
-#include <vector>
 #include "ReplacementSelectionSort.h"
-#include "DualHeap.h"
-//#include "ReplacementSelectionSort.cpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <random>
+#include <cmath>
+#include <sstream>
+#include <conio.h>
+#include <stdio.h>
 
 using namespace std;
 
 template <typename T>
-void replacementSelectionSortRun( const std::vector<T> SAMPLE_DATA, const int HEAP_SIZE);
+void test(string inputFileName, string outputFileName, bool direction_flag);
 
 
+template<typename T>
+void generateTestFile(ostream& outfile);
 
-int main()
-{
-    //push sample data to vector
-    std::vector<std::string> sampleData;
-    sampleData.push_back("apples");
-    sampleData.push_back("oranges");
-    sampleData.push_back("bananas");
-    sampleData.push_back("cucumbers");
-    sampleData.push_back("ducks");
-    sampleData.push_back("iguanas");
-    sampleData.push_back("zebras");
-    sampleData.push_back("porcupines");
-    sampleData.push_back("quills");
-    sampleData.push_back("fingers");
-    sampleData.push_back("toes");
-    sampleData.push_back("bows");
-    sampleData.push_back("arrows");
-    sampleData.push_back("swords");
-    sampleData.push_back("helmets");
-    sampleData.push_back("cheese");
+template <typename T>
+void checkRuns(istream& infile,ostream& outfile, bool direction_flag);
 
 
+int main(){
+
+    srand(2);
 
 
+	test<int> ("randomIntTest_in.txt","ascending_randomIntTest_out", true);
 
 
-    for (int i = 0; i < sampleData.size(); i++){
-        cout << sampleData[i] << endl;
-    }
-
-    replacementSelectionSortRun(sampleData, 10);
-
-    cout<< endl;
-    for (int i = 0; i < sampleData.size(); i++){
-        cout << sampleData[i] << endl;
-    }
-
-    return 0;
+	return 0;
 }
 
-    template <typename T>
-    void replacementSelectionSortRun( const std::vector<T> SAMPLE_DATA, const int HEAP_SIZE)
-    {
+template <typename T>
+void test(string inputFileName, string outputFileName, bool direction_flag){
 
-        std::vector< std::vector<T> > results = ReplacementSelectionSort<T>::sort( SAMPLE_DATA, HEAP_SIZE, true );
+	ofstream randomTestFile(inputFileName);
 
-        cout<< endl;
-        for (int i = 0; i < results.size(); i++)
-        {
-            //cout << i <<": "<< results[i].size() << endl;
+	cout << "Creating test file\n";
+	generateTestFile<T>(randomTestFile);
+	randomTestFile.close();
 
-            for (int j = 0; j < results[i].size(); j++)
-            {
-                cout << "results" << "[" << i << "]" << "[" << j << "]" <<results[i][j] << endl;
+	ifstream inputFile(inputFileName);
+	ofstream otemp("temp.txt");
+	cout << "rs\n";
 
-            }
-        }
+	ReplacementSelectionSort<T> sortedRuns(15, inputFile ,otemp, direction_flag);
+	otemp.close();
 
-    }
+	ifstream itemp("temp.txt");
+	ofstream outputFile(outputFileName);
 
+	cout << "out\n";
+	checkRuns<T>(itemp, outputFile, direction_flag);
+}
+
+template<typename T>
+void generateTestFile(ostream& outfile){
+
+	int strlength;
+	int fileLen;
+	fileLen = rand() % (rand() % 1000);
+
+	for(int i = 0; i < fileLen; i++){
+
+		strlength = 1 + rand()% 3;
+
+		T num = rand() % (int)(pow(10,strlength));
+
+		outfile << num <<" ";
+		if(i % 5 == 0){
+			outfile << "\n";
+			}
+
+		}
+}
+
+
+template <typename T>
+void checkRuns(istream& infile,ostream& outfile, bool direction_flag){
+
+
+	int runLength = 0;
+
+	int maxRunLength = 0;
+
+
+	int totalObj = 0;
+
+	int totalRuns = 0;
+
+	bool isSorted = true;
+
+	T prevObj;
+	T curObj;
+
+	string curLine;
+
+	while(getline(infile, curLine)){
+
+		runLength = 0;
+		outfile << curLine<<"\n";
+		totalRuns++;
+		istringstream stringStream(curLine);
+		stringStream >> prevObj;
+		runLength++;
+		totalObj++;
+
+		while(!stringStream.eof()){
+
+			stringStream>>curObj;
+			runLength++;
+			totalObj++;
+			if(direction_flag){
+
+				if(curObj < prevObj){
+
+					isSorted = false;
+				}
+			}else{
+
+				if(curObj > prevObj){
+					isSorted = false;
+				}
+
+			}
+		}
+		if(runLength > maxRunLength){
+			maxRunLength = runLength;
+		}
+	}
+
+	outfile <<"\n\n-----------------------------------\n"
+			<<"averageRunLength = " << totalObj / totalRuns
+			<<"\nthe file is sorted: "<< (isSorted? "True" : "False")
+			<<"\nMax run length: " << maxRunLength
+			<<"\ntotal Runs: " << totalRuns
+			<<"\ntotal items: " << totalObj
+			<<"\n---------------------------------------\n\n";
+}
