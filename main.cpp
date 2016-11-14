8dbbea06c177db0d22ee8fb8de2579a9b8facc47
Steven K Scholz <stevenkurtscholz@gmail.com>
2016-11-13 23:43:50 -0600

    added merge for ints only

--- main.cpp
+++ main.cpp
@@ -34,265 +34,7 @@ void makeVector(std::vector < std::vector<T> > listVector);
 
 int stringToNumber(string inString);
 
-
-// Functionality:
-// To randomly populate a given number of sorted integers.
-// This can be used as input data set.
-// Input:
-//   vector<int>& v  - Vector on which sorted integers will be populated
-//   size         - Number of integers to populate
-// Output:
-//   void
-
-void populateSortedVector(vector<int>& v, int size)
-{
-   if (size <= 0) {
-      return;
-   }
-
-   v.clear();
-   for(int i = 0; i < size; i++) {
-      v.push_back(rand());
-   }
-
-   sort(v.begin(), v.end());
-}
-
-
-//   Functionality:
-//      Merge K sorted arrays using TOURNAMENT TREE
-//
-//   Input:
-//      vector <vector<int>>& sortedArrays - K sorted arrays
-//   Output:
-//      vector <vector<int>>& sortedArrays - One array which is result of merging K sorted arrays
-//
-
-void mergeSortedArraysUsingTournamentTree(vector <vector<int>>& sortedArrays)
-{
-   if (sortedArrays.size() == 0) {
-      return;
-   }
-
-   removeEmptyVectors<int>(sortedArrays);
-
-   vector<int> mergedSortedArray;
-   vector<elementIndex<int, int>> tournamentTree;
-   int firstLeaf = 0, vIndex = 0, numOfPlayers = sortedArrays.size();
-
-   buildTournamentTree(tournamentTree, firstLeaf, sortedArrays);
-   playTournament(tournamentTree, firstLeaf);
-
-   while (numOfPlayers) {
-      int vIndex = tournamentTree[0].arrayIndex, curPos = -1, value;
-      elementIndex<int, int> tournamentWinner = tournamentTree[0];
-
-      //   1) Get winner and add that to output array
-      mergedSortedArray.push_back(tournamentWinner.value);
-      //   2) Fetch next data
-      curPos = tournamentWinner.position;
-      if (fetchNextData(sortedArrays[vIndex], curPos, value)) {
-         elementIndex<int, int> newPlayer(value, vIndex, curPos, tournamentWinner.metaData);
-         // Add this new player at the position of the last Game's winner
-         addPlayerAt(tournamentTree, newPlayer);
-      }
-      else { // If no more elements in the current vector then make this player an automatic loser
-         --numOfPlayers;
-         tournamentTree[tournamentWinner.metaData].arrayIndex = -1;
-         elementIndex<int, int> newPlayer(value, -1, -1, tournamentWinner.metaData);
-         addPlayerAt(tournamentTree, newPlayer);
-      }
-   }
-
-   sortedArrays.clear();
-   sortedArrays.push_back(mergedSortedArray);
-}
-
-
-
-void testTournamentTreeMethod()
-{
-   vector<int> v;
-   int i = 0, count = 5;
-   vector <vector<int>> vectorOfSortedArrays;
-
-   srand((unsigned)time(NULL));
-   for (; i < 25; i++, count+=15) {
-      populateSortedVector(v, count);
-      vectorOfSortedArrays.push_back(v);
-   }
-
-   mergeSortedArraysUsingTournamentTree(vectorOfSortedArrays);
-   if (is_sorted(vectorOfSortedArrays[0])) {
-      cout<<"K Sorted Arrays are successfully merged using TOURNAMENT TREE mehtod, complexity is NLOGK"<<endl;
-   }
-}
-
-
-
-
-
-
-
-/ Functionality:
-//   Creates a tournament tree. Internal representation is using array/vector.
-//   Represents the complete binary tree using array.
-//
-//   Input:
-//      vector<elementIndex<int, int>>& tournamentTree - Tournament tree to be populated
-//      int& firstLeaf   - Index of the first player in the last level
-//      const vector<vector<int>>& sortedArrays - vector of sorted arrays
-//
-
-void buildTournamentTree(vector<elementIndex<int, int>>& tournamentTree, int& firstLeaf, const vector<vector<int>>& sortedArrays)
-{
-   if (sortedArrays.size() == 0) {
-      return;
-   }
-
-   int size = 1, tournamentTreeSize = 0, vIndex = 0;
-
-   while (size < sortedArrays.size()) {
-      size*=2;
-   }
-
-   firstLeaf = size - 1;
-   tournamentTreeSize = (2 * size) - 1;
-   tournamentTree.resize(tournamentTreeSize);
-
-   for (int i = firstLeaf; vIndex < sortedArrays.size(); i++) {
-      tournamentTree[i].value = sortedArrays[vIndex].at(0);
-      tournamentTree[i].arrayIndex = vIndex;
-      tournamentTree[i].position = 1;
-      tournamentTree[i].metaData = i;
-      ++vIndex;
-   }
-}
-
-
-
-
-//   Functionality:
-//      Assumption:
-//         The given tournament tree is expected to be the complete binary tree
-//   Given a Tournament tree and the index of the first player, conducts a tournament
-//   A match is played against a pair of players and the winner is promoted to the next level.
-//   Input:
-//      vector<elementIndex<int, int>>& tournamentTree - Tournament tree containing players information
-//      int firstLeaf                           - Index of the first player in the leaf level of the complete Binary tree
-//
-//   Output:
-//      void
-
-void playTournament(vector<elementIndex<int, int>>& tournamentTree, int firstLeaf)
-{
-   int thisLevelLeftMostPlayer = firstLeaf;
-
-   while (thisLevelLeftMostPlayer > 0) {
-      int left = thisLevelLeftMostPlayer, thisLevelRigthMostPlayer = 0, winner = -1, parent = 0;
-      thisLevelRigthMostPlayer = 2 * left;
-
-      while (left < thisLevelRigthMostPlayer) {
-         winner = playMatch(tournamentTree[left], tournamentTree[left+1]);
-         if (winner <= 0) {
-            break;
-         }
-         parent = left/2;
-         tournamentTree[parent] = tournamentTree[winner];
-         left+=2;
-      }
-      thisLevelLeftMostPlayer = (thisLevelLeftMostPlayer - 1)/2;
-   }
-
-}
-
-
-
-
-
-//   Functionality:
-//      Assumption:
-//         The given tournament tree is expected to be the complete binary tree
-//
-//      Given a tournament tree, insert a new player at certain position (indicated by newPlayer.metaData)
-//      and play the tournament with the rest of the players
-//      This will promote the winner to the next level along the path till it reaches the root
-//      At the most log n matches (considering there are n players)
-//
-//      Input:
-//         vector<elementIndex<int, int>>& tournamentTree - Tournament tree with players information
-//         const elementIndex<int, int>& newPlayer        - New player information
-
-void addPlayerAt(vector<elementIndex<int, int>>& tournamentTree, const elementIndex<int, int>& newPlayer)
-{
-   if(tournamentTree.size() == 0) {
-      return;
-   }
-
-   int playerPosition = newPlayer.metaData;
-
-   tournamentTree[newPlayer.metaData] = newPlayer;
-   while (playerPosition > 0) {
-      int player1, player2, winner, parent;
-      player1 = (playerPosition % 2 ? playerPosition : playerPosition - 1);
-      player2 = player1 + 1;
-      parent = player1/2;
-      winner = playMatch(tournamentTree[player1], tournamentTree[player2]);
-      tournamentTree[parent] = ((winner != -1) ? tournamentTree[winner] : tournamentTree[player1]);
-      playerPosition = parent;
-   }
-
-}
-
-
-
-/ Functionality:
-//   Given two player's meta data, finds the winner
-//
-//   Input:
-//      elementIndex& player1 - Player 1's data
-//      elementIndex& player2 - Player 2's data
-//   Output:
-//      Player (array) index in the tournament tree who is the Winner
-//   Caution:
-//      If both the players are Automatic losers, then the player index will be returned as negative value
-//      Callers should check the return value before directly accessing the tournament tree using this player index
-
-int playMatch(const elementIndex<int, int>& player1, const elementIndex<int, int>& player2)
-{
-   if ((player1.arrayIndex >= 0) && (player2.arrayIndex >= 0)) {
-      return player1.value < player2.value ? player1.metaData : player2.metaData;
-   }
-   else if (player1.arrayIndex < 0) {
-      return player2.metaData;
-   }
-
-   return player1.metaData;
-}
-
-
-
-
-void testTournamentTreeMethod()
-{
-   vector<int> v;
-   int i = 0, count = 5;
-   vector <vector<int>> vectorOfSortedArrays;
-
-   srand((unsigned)time(NULL));
-   for (; i < 25; i++, count+=15) {
-      populateSortedVector(v, count);
-      vectorOfSortedArrays.push_back(v);
-   }
-
-   mergeSortedArraysUsingTournamentTree(vectorOfSortedArrays);
-   if (is_sorted(vectorOfSortedArrays[0])) {
-      cout<<"K Sorted Arrays are successfully merged using TOURNAMENT TREE mehtod, complexity is NLOGK"<<endl;
-   }
-}
-
-
-
+vector<int> mergeInt(const vector<vector<int> >& multiList);
 
 int main(){
 
@@ -315,63 +57,90 @@ int main(){
 
 
 
-vector<Run> runVector = initRuns<int> ("out.txt");
+    std::vector < std::vector<int> > testVector;
+
+    ifstream inFile("out.txt");
+    string line;
+
+
+    while(getline(inFile, line, ' ')){
+
+        std::stringstream ss(line);
+
+        std::vector<int> numbers;
+        std::string in_line;
+        int temp;
+        while(getline (ss, in_line)){
+            std::stringstream iss(in_line);
+            iss >> temp;
+            numbers.push_back(temp);
+        }
+        testVector.push_back(numbers);
+    }
+
+
+    for(int i = 0; i < testVector.size(); i++){
+
+        for (int j = 0; j < testVector[i].size(); j++){
+
+        cout << testVector[i][j];
+
+        }
+
+    }
+
+
+ cout << endl << endl << endl;
 
 
 
 
+vector<int> finalVector;
+
+finalVector = mergeInt(testVector);
+
+    for(int i = 0; i < finalVector.size(); i++){
+
+        cout << finalVector[i] << endl;
+    }
 
 	return 0;
 }
 
 
-vector<int> mergeDivideAndConquer(vector<vector<int> > multiList) {
-  int l = 0, r = multiList.size()-1;
 
-  while (multiList.size() > 1) {
-    multiList[l] = mergeLists(multiList[l], multiList[r]);
-    multiList.pop_back();
 
-    l++;
-    r = multiList.size()-1;
 
-    if (r <= l) l = 0;
+vector<int> mergeInt(const vector<vector<int> >& multiList) {
+
+  vector<int> finalList;
+  vector<vector<int>::const_iterator> iterators(multiList.size());
+
+  // Set all iterators to the beginning of their corresponding vectors in multiList
+  for (int i = 0; i < multiList.size(); ++i) iterators[i] = multiList[i].begin();
+
+  int k = 0, minValue, minValueIndex;
+
+  while (1) {
+    minValue = INT_MAX;
+    for (int i = 0; i < iterators.size(); ++i){
+      if (iterators[i] == multiList[i].end()) continue;
+
+      if (*iterators[i] < minValue) {
+        minValue = *iterators[i];
+        minValueIndex = i;
+      }
+    }
+
+    iterators[minValueIndex]++;
+
+    if (minValue == INT_MAX) break;
+    finalList.push_back(minValue);
   }
 
-  return multiList[0];
+  return finalList;
 }
 
-
-
-
-
-//int stringToNumber(string inString){
-//
-//    int Number;
-//
-//    if ( ! (istringstream(inString) >> Number) ) Number = 0;
-//    return Number;
-//
-//}
-//
-//
-//template <typename T>
-//void makeVector(std::vector < std::vector<T> > listVector){
-//
-//    ifstream inFile("out.txt");
-//    std::string element;
-//    std::vector <T> temp;
-//    int intElement;
-//
-//    std::vector < std::vector <T> > listVector;
-//
-//        while(getline(inFile, element)){
-//
-//
-//        }
-//
-//
-//}
 
 template <typename T>
 void test(string inputFileName, string outputFileName, bool direction_flag){
